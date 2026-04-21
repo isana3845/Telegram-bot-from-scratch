ba3733d3e84db298776c93727c16009bf129e6aa
Kembi0736 <welkome1772@gmail.com>
2026-04-21 19:20:12 +1000

    Добавил отслеживание текущей недели

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+from datetime import datetime, timedelta
+
+
+url = "https://univer.dvfu.ru/schedule/get"
+
+date = datetime.now()
+start = date - timedelta((date.weekday() + 1) % 7)
+end = start + timedelta(6)
+
+params = {
+    "type": "agendaWeek",
+    "start": f"{start.strftime('%Y-%m-%d')}T14:00:00.000Z",
+    "end": f"{end.strftime('%Y-%m-%d')}T14:00:00.000Z",
+    "groups[]": "6886",
+    "ppsGuid": "",
+    "facilityId": 0
+}
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "ru-RU,ru;q=0.9,en;q=0.8",
+    "Referer": "https://univer.dvfu.ru/schedule",
+    "Origin": "https://univer.dvfu.ru",
+    "X-Requested-With": "XMLHttpRequest"
+}
+
+response = requests.get(url, params=params, headers=headers)
+
+try:
+    for i in range(len(response.json()["events"])):
+        resp = response.json()["events"][i]
+        print(f"{resp["start"][11:16]}-{resp["end"][11:16]}\t{resp["pps_load"]}\n{resp["title"]}\t{resp["classroom"]}\n")
+except Exception as e:
+    print(e)
